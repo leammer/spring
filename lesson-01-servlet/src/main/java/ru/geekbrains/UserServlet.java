de0ae66e0c4c652aa17f1c9b740c776bf99560d0
Valentina <wasiljeva@gmail.com>
2022-05-29 13:25:20 +0300

    Revert "added incomming code"
    This reverts commit 6b499dce3be13fbeb1d3775f8e653606d6ed791c.

--- lesson-01-servlet/src/main/java/ru/geekbrains/UserServlet.java
+++ lesson-01-servlet/src/main/java/ru/geekbrains/UserServlet.java
@@ -8,64 +8,75 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @WebServlet(urlPatterns = "/user/*")
 public class UserServlet extends HttpServlet {
 
-    private static final Pattern PARAM_PATTERN = Pattern.compile("\\/(\\d+)");
+	private final String BAD_REQUEST_MESSAGE = "The request must contain the user number in a numeric value";
+	private final String NOT_FOUND_MESSAGE = "There is no such user";
 
-    private UserRepository userRepository;
+	private UserRepository userRepository;
 
-    @Override
-    public void init() throws ServletException {
-        this.userRepository = new UserRepository();
-        userRepository.insert(new User("User 1"));
-        userRepository.insert(new User("User 2"));
-        userRepository.insert(new User("User 3"));
-    }
+	@Override
+	public void init() throws ServletException {
+		this.userRepository = new UserRepository();
+		userRepository.insert(new User("User 1"));
+		userRepository.insert(new User("User 2"));
+		userRepository.insert(new User("User 3"));
+	}
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if (req.getPathInfo() == null || req.getPathInfo().equals("/")) {
-            req.setAttribute("users", userRepository.findAll());
-            getServletContext().getRequestDispatcher("/user.jsp").forward(req, resp);
-        }
-//        if (req.getPathInfo() == null || req.getPathInfo().equals("/")) {
-//            PrintWriter wr = resp.getWriter();
-//            wr.println("<table>");
-//            wr.println("<tr>");
-//            wr.println("<th>Id</th>");
-//            wr.println("<th>Username</th>");
-//            wr.println("</tr>");
-//
-//            for (User user : userRepository.findAll()) {
-//                wr.println("<tr>");
-//                wr.println("<td><a href='" + req.getContextPath() + "/user/" + user.getId() + "'>" + user.getId() + "</a></td>");
-//                wr.println("<td>" + user.getUsername() + "</td>");
-//                wr.println("</tr>");
-//            }
-//
-//            wr.println("</table>");
-//        } else {
-//            Matcher matcher = PARAM_PATTERN.matcher(req.getPathInfo());
-//            if (matcher.matches()) {
-//                long id = Long.parseLong(matcher.group(1));
-//                User user = this.userRepository.findById(id);
-//                if (user == null) {
-//                    resp.getWriter().println("User not found");
-//                    resp.setStatus(404);
-//                    return;
-//                }
-//                resp.getWriter().println("<p>Id: " + user.getId() + "</p>");
-//                resp.getWriter().println("<p>Username: " + user.getUsername() + "</p>");
-//            } else {
-//                resp.getWriter().println("Bad parameter value");
-//                resp.setStatus(400);
-//            }
-//        }
-    }
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter wr = resp.getWriter();
+
+		if (req.getPathInfo() == null || req.getPathInfo().equals("/")) {
+			printAllUsers(wr);
+			return;
+		}
+
+		Long id = getIdFromInfo(req.getPathInfo());
+		if (id == -1) {
+			resp.sendError(HttpServletResponse.SC_BAD_REQUEST, BAD_REQUEST_MESSAGE);
+			return;
+		}
+
+		User user = userRepository.findById(id);
+		if (user == null) {
+			resp.sendError(HttpServletResponse.SC_NOT_FOUND, NOT_FOUND_MESSAGE);
+			return;
+		}
+			
+		wr.println("<p>User name: " + user.getUsername() + "</p>");
+	}
+
+	private void printAllUsers(PrintWriter wr) {
+		wr.println("<h1>List of users</h1>");
+
+		wr.println("<table>");
+		wr.println("<tr>");
+		wr.println("<th>Id</th>");
+		wr.println("<th>Username</th>");
+		wr.println("</tr>");
+
+		for (User user : userRepository.findAll()) {
+			wr.println("<tr>");
+			wr.println("<td>" + user.getId() + "</td>");
+			wr.println("<td>" + user.getUsername() + "</td>");
+			wr.println("</tr>");
+		}
+
+		wr.println("</table>");
+	}
+
+	private static Long getIdFromInfo(String str) {
+		try {
+			Long id = Long.parseLong(str.substring(1));
+			return id;
+		} catch (NullPointerException | NumberFormatException e) {
+			return (long) -1;
+		}
+	}
 }
